4524a77d2003004a9c8b77c30e6b81b42d8a1fe4
Romain Brenguier <romain.brenguier@gmail.com>
2015-05-08 11:23:22 +0200

    Installation scripts

--- install_ocaml_aiger.sh
+++ install_ocaml_aiger.sh
@@ -3,21 +3,19 @@
 echo "Installing Ocaml-aiger ..."
 
 OCAML_AIGER="ocaml-aiger"
-OCAML_AIGER_ARCHIVE="$OCAML_AIGER.tar.gz"
+OCAML_AIGER_ARCHIVE="$OCAML_AIGER.zip"
 
 if [ ! -e "$OCAML_AIGER_ARCHIVE" ];
 then
   echo " Downloading Ocaml-aiger utilities ..."
-  #wget http://www.ulb.ac.be/di/verif/rbrengui/ocaml-aiger/$OCAML_AIGER_ARCHIVE
   wget https://github.com/romainbrenguier/ocaml-aiger/archive/master.zip
 fi
 
 echo " Unpacking Ocaml-aiger ..."
 rm -rf $OCAML_AIGER
-mkdir $OCAML_AIGER
-#tar -xzf $OCAML_AIGER_ARCHIVE
-mv master.zip $OCAML_AIGER.zip
-unzip $OCAML_AIGER.zip
+mv master.zip $OCAML_AIGER_ARCHIVE
+unzip $OCAML_AIGER_ARCHIVE
+mv $OCAML_AIGER-master $OCAML_AIGER
 
 echo " Compiling Ocaml-aiger ..."
-(cd $OCAML_AIGER; make)
+(cd $OCAML_AIGER-master; make)
--- install_ocaml_cudd.sh
+++ install_ocaml_cudd.sh
@@ -3,21 +3,19 @@
 echo "Installing Ocaml-CUDD..."
 
 OCAML_CUDD="ocaml-cudd"
-OCAML_CUDD_ARCHIVE="$OCAML_CUDD.tar.gz"
+OCAML_CUDD_ARCHIVE="$OCAML_CUDD.zip"
 
-if [ ! -e "$CUDD_ARCHIVE" ];
+if [ ! -e "$OCAML_CUDD_ARCHIVE" ];
 then
   echo " Downloading Ocaml-CUDD..."
-  #wget http://www.ulb.ac.be/di/verif/rbrengui/ocaml-cudd/$OCAML_CUDD_ARCHIVE
   wget https://github.com/romainbrenguier/ocaml-cudd/archive/master.zip
 fi
 
 echo " Unpacking Ocaml-CUDD..."
 rm -rf $OCAML_CUDD
-mkdir $OCAML_CUDD
-#tar -xzf $OCAML_CUDD_ARCHIVE
-mv master.zip $OCAML_CUDD.zip
-unzip $OCAML_CUDD.zip
+mv master.zip $OCAML_CUDD_ARCHIVE
+unzip $OCAML_CUDD_ARCHIVE
+mv $OCAML_CUDD-master $OCAML_CUDD-master
 
 echo " Compiling Ocaml-CUDD..."
-(cd $OCAML_CUDD; ./install_cudd.sh; make)
+(cd $OCAML_CUDD-master; ./install_cudd.sh; make)
