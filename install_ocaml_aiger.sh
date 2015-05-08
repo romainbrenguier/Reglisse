d21dad53c77ea3e95cbe975328e729ed141cde3c
Romain Brenguier <romain.brenguier@gmail.com>
2015-05-08 11:23:01 +0200

    Installation scripts

--- install_ocaml_aiger.sh
+++ install_ocaml_aiger.sh
@@ -8,13 +8,16 @@ OCAML_AIGER_ARCHIVE="$OCAML_AIGER.tar.gz"
 if [ ! -e "$OCAML_AIGER_ARCHIVE" ];
 then
   echo " Downloading Ocaml-aiger utilities ..."
-  wget http://www.ulb.ac.be/di/verif/rbrengui/ocaml-aiger/$OCAML_AIGER_ARCHIVE
+  #wget http://www.ulb.ac.be/di/verif/rbrengui/ocaml-aiger/$OCAML_AIGER_ARCHIVE
+  wget https://github.com/romainbrenguier/ocaml-aiger/archive/master.zip
 fi
 
 echo " Unpacking Ocaml-aiger ..."
 rm -rf $OCAML_AIGER
 mkdir $OCAML_AIGER
-tar -xzf $OCAML_AIGER_ARCHIVE
+#tar -xzf $OCAML_AIGER_ARCHIVE
+mv master.zip $OCAML_AIGER.zip
+unzip $OCAML_AIGER.zip
 
 echo " Compiling Ocaml-aiger ..."
 (cd $OCAML_AIGER; make)
--- install_ocaml_cudd.sh
+++ install_ocaml_cudd.sh
@@ -8,13 +8,16 @@ OCAML_CUDD_ARCHIVE="$OCAML_CUDD.tar.gz"
 if [ ! -e "$CUDD_ARCHIVE" ];
 then
   echo " Downloading Ocaml-CUDD..."
-  wget http://www.ulb.ac.be/di/verif/rbrengui/ocaml-cudd/$OCAML_CUDD_ARCHIVE
+  #wget http://www.ulb.ac.be/di/verif/rbrengui/ocaml-cudd/$OCAML_CUDD_ARCHIVE
+  wget https://github.com/romainbrenguier/ocaml-cudd/archive/master.zip
 fi
 
 echo " Unpacking Ocaml-CUDD..."
 rm -rf $OCAML_CUDD
 mkdir $OCAML_CUDD
-tar -xzf $OCAML_CUDD_ARCHIVE
+#tar -xzf $OCAML_CUDD_ARCHIVE
+mv master.zip $OCAML_CUDD.zip
+unzip $OCAML_CUDD.zip
 
 echo " Compiling Ocaml-CUDD..."
 (cd $OCAML_CUDD; ./install_cudd.sh; make)
