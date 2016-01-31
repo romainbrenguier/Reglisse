a082afaf95813148e079335c7c462b331bd1d623
Romain Brenguier <romain.brenguier@cs.ox.ac.uk>
2016-01-31 11:23:36 +0000

    Test script

--- examples/test.sh
+++ examples/test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 echo "=== Classical ==="
-for i in examples/*.rgl; do echo "Opening file " $i; ./reglisse -t $i; done
-echo "=== Cooperative ==="
-for i in examples/*.rgl; do echo "Opening file " $i; ./reglisse -t -m 1 $i; done
-echo "=== Adversarial ==="
-for i in examples/*.rgl; do echo "Opening file " $i; ./reglisse -t -m 2 $i; done
-echo "=== Admissible ==="
-for i in examples/*.rgl; do echo "Opening file " $i; ./reglisse -t -m 3 $i; done
+for i in examples/*.rgl; do
+    echo "Classical " $i;
+    ./reglisse -t $i; 
+    echo "Cooperative " $i;
+    ./reglisse -t -m 1 $i; 
+    echo "Adversarial " $i;
+    ./reglisse -t -m 2 $i; 
+    echo "Admissible " $i;
+    ./reglisse -t -m 3 $i; 
+done
+
 
